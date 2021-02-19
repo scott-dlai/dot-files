65e083908bb110a608048cc3d530445c21dae818
Scott Lai <tuonglai3602@gmail.com>
2021-02-18 20:54:58 -0800

    Changed prompt.

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -1,4 +1,5 @@
 # Prompt
 export PS1="
-%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%m %{$fg[magenta]%}%1~%{$fg[red]%}]%{$reset_color%}$%b "
+%B┌%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[red]%}]%{$reset_color%}─%B%{$fg[red]%}[%{$fg[blue]%}%1~%{$fg[red]%}]
+%{$reset_color%}└─$ %b"
 
