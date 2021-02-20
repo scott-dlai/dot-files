6c8c95c65ef507b44c78f725c38b61eb975f489d
Scott Lai <tuonglai3602@gmail.com>
2021-02-19 19:42:13 -0800

    Added version of prompt.

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -1,5 +1,9 @@
 # Prompt
 export PS1="
 %B%{$fg[cyan]%}┌%{$fg[cyan]%}[%{$fg[yellow]%}%n%{$fg[cyan]%}]%{$fg[cyan]%}─%{$fg[cyan]%}[%{$fg[blue]%}%~%{$fg[cyan]%}]
-%{$fg[cyan]%}└─%{$fg[cyan]%}❯ %b"
+└─❯ %b"
+
+# export PS1="
+# %B┌[%{$fg[yellow]%}%n%{$reset_color%}]─[%{$fg[blue]%}%~%{$reset_color%}]
+# └─❯ %b"
 
